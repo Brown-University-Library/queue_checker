2bc161befb25dea98a1026c1a6b679565e567b3e
Birkin James Diana <birkin.diana@gmail.com>
2023-05-11 11:55:55 -0400

    adds docstest docstring.

--- queue_check.py
+++ queue_check.py
@@ -11,7 +11,7 @@ Usage:
 % python ./queue_check.py
 """
 
-import logging, subprocess
+import logging, pprint, subprocess
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -30,8 +30,50 @@ def run_code():
     """
     output: str  = get_rqinfo()
     assert type(output) == str
-    return_dct: dict = {}
-    return return_dct
+    data_dct: dict = parse_rqinfo( output )
+    return data_dct
+
+
+def parse_rqinfo( rq_output: str ) -> dict:
+    """ 
+    Parses rqinfo output into a dict.
+
+    Usage:
+    >>> parse_rqinfo(
+    ...     'queue q_1 0\\n'
+    ...     'queue q_2 0\\n'
+    ...     'queue failed 333\\n'
+    ...     'q_1: server.968 (idle), server.952 (idle)\\n'
+    ...     'q_2: server.952 (idle)\\n'
+    ...     'failed: –\\n'
+    ... )
+    {'failed_count': 333, 'queues': ['q_1', 'q_2', 'failed'], 'workers_by_queue': {'q_1': ['server.968', 'server.952'], 'q_2': ['server.952'], 'failed': []}}
+    """
+    lines = rq_output.split('\n')
+    log.debug( f'lines, ``{lines}``' )
+    output = {'failed_count': 0, 'queues': [], 'workers_by_queue': {}}
+    for line in lines:
+        log.debug( f'processing line, ``{line}``' )
+        line = line.strip()
+        if line == '':
+            log.debug( 'blank line; continuing' )
+            continue
+        if line.startswith('queue'):    # Line format: queue <queue_name> <count>
+            ( _, queue_name, count ) = line.split()
+            output['queues'].append(queue_name)
+            if queue_name == 'failed':
+                output['failed_count'] = int(count)
+        else:                           # Line format: <queue_name>: <worker.123 (idle), worker.124 (idle)> ...or...
+                                        #              failed: –
+            ( queue_name, worker_data ) = line.split(':')
+            worker_data = worker_data.strip()
+            worker_names = []
+            if worker_data != '–':      # Split by comma and get the server name from each part
+                worker_names = [part.split()[0] for part in worker_data.split(',')]
+            output['workers_by_queue'][queue_name] = worker_names
+    log.debug( f'output, ``{pprint.pformat(output)}``' )
+    return output
+
 
 
 def get_rqinfo() -> str:
@@ -102,6 +144,8 @@ def get_rqinfo() -> str:
 #             workers.append(worker_name)
 
 #     return redis_check, queues, workers
+
+
 
 
 if __name__ == '__main__':
